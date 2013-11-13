ea283af9ccec9b583afc1c55a6b4cac6455b9d07
Pranjal Garg <garg.pranjal2@gmail.com>
2013-11-13 09:09:36 -0800

    Improved the readibilty and seperate function baseline function for Css

--- VerticalRhythm.hs
+++ VerticalRhythm.hs
@@ -1,29 +1,46 @@
+{-
+
+A Module to implement Vertical Rhythm in Css generated using clay
+
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+
 module VerticalRhythm where
 import Clay
 import Control.Monad.State
 
+-- | Type CSSState used to keep track of the base_font_size and the base_line_height
 type CSSState = (Integer,Integer)
-
 type MyStateMonad  = State CSSState 
 
+
+-- |  Establishes the baseline for the given CSSState
 establish_baseline :: MyStateMonad Css
 establish_baseline = state (\st ->
 				let (x , y) = st 
-				    k=rhythm 1.0 x 0
-				    p= (realToFrac (2*100)) / (realToFrac 16)
-				    c= ( do 
-			     
-		 	          		html?
-				       			fontSize ( pct p)      
-				  		html ?
-				    			do
-				       				fontSize      (px x)
-				       				lineHeight (em k))
+				    c= baseline x y
+				in (c, st))
 
-		       		in (c, st))
-rhythm:: Double->Integer->Integer->Double
-rhythm lines font_size offset  =do 	let h = 2
-					((lines*realToFrac((h - offset))) / realToFrac (font_size))
 
+-- | returns baseline Css
+baseline::Integer->Integer->Css
+baseline x y = do 
+	--setting base Css
+	-- x is the base_font_size and y is the base_line_height 		     
+	html?
+	   fontSize ( pct p)      
+	html ? do
+	   fontSize      (px x)
+	   lineHeight (em k)
+	where
+	k=rhythm 1.0 x y 0
+	p= (realToFrac (2*100)) / (realToFrac 16)
+
+
+--  | Calculates rhythm units
+rhythm:: Double -> Integer-> Integer -> Integer -> Double
+rhythm l f h o  = do ((l*realToFrac((h - o))) / realToFrac (f))
+--l is the number of lines, f is the base_font_size, h is the base_line_height and o is the offset
 
 
