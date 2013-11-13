ec2cb94eab7cd26f5d4e91cc502d481c304901a5
Pranjal Garg <garg.pranjal2@gmail.com>
2013-11-13 08:18:48 -0800

    used Control.Monad.State Module to implement the state function

--- VerticalRhythm.hs
+++ VerticalRhythm.hs
@@ -1,27 +1,13 @@
 module VerticalRhythm where
 import Clay
-
-data Config a = Co a 
-
-
-newtype State s a = State (s -> (a,s))
+import Control.Monad.State
 
 type CSSState = (Integer,Integer)
 
-instance Monad (State s) where
+type MyStateMonad  = State CSSState 
 
-  return x = State ( \st -> (x, st) )
-  mv >>= g = State ( \st -> 
-                 let (y, st') = runState mv st
-                 in runState (g y) st' )
-
-
-
-runState :: State s a -> s -> (a, s)
-runState (State f) init_st = f init_st
-
-establish_baseline :: State CSSState Css
-establish_baseline = State(\st ->
+establish_baseline :: MyStateMonad Css
+establish_baseline = state (\st ->
 				let (x , y) = st 
 				    k=rhythm 1.0 x 0
 				    p= (realToFrac (2*100)) / (realToFrac 16)
--- test.hs
+++ test.hs
@@ -1,5 +1,6 @@
 import VerticalRhythm
 import Clay
+import Control.Monad.State
 
 main =  let k= fst(runState establish_baseline (5,4))
 	in putCss k
