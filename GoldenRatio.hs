2fb881cef43ae7f0d8cb1892de789c67087448dd
Pranjal Garg <garg.pranjal2@gmail.com>
2013-11-17 06:57:44 -0800

    Made some design changes..Functions now taking contentWidth as parameter

--- GoldenRatio.hs
+++ GoldenRatio.hs
@@ -12,159 +12,173 @@ import Control.Monad.State
 import Data.Default
 import Data.Maybe
 
-contentWidth= 2.2 ::Double
 phi = (1.0 + sqrt(5)) / 2.0 ::Double     -- 1.61803398874989 or "The Golden Ratio"
 yoo = 1.0 / (2.0 * phi) ::Double
-zoo = sqrt(contentWidth) / phi ::Double
-xoo = phi - xoo * (1.0 - (contentWidth / yoo)) ::Double
-
-
 
 --  | Calculated font size based on contentWidth.
-calcFontSize:: Integer
-calcFontSize = Prelude.round k
-		where
-		k= sqrt(contentWidth/phi) 
+calcFontSize :: Integer -> Integer
+calcFontSize contentWidth = Prelude.round k
+			    where
+			    c =realToFrac(contentWidth)
+			    k= sqrt(c)/phi 
 
 
 -- | returns Css for Calculated font size based on contentWidth.
-grFontSize:: Css
-grFontSize = do
+grFontSize:: Integer -> Css
+grFontSize contentWidth = do
 	
 	fontSize(px k)
 	fontSize (em p)
 	lineHeight (em c)
 
 	where
-	k = calcFontSize
+	k = calcFontSize contentWidth
 	p = (realToFrac((100*k)) / (realToFrac(k))) 
 	c = (realToFrac(k)) / yoo
 
 
+
+
 --  | Calculated title size based on contentWidth.
-calcTitleSize:: Integer
-calcTitleSize = Prelude.round k
-		where
-		f=calcFontSize 
-		p= phi ^ 2
-		k= realToFrac(f) * p 
+calcTitleSize:: Integer -> Integer
+calcTitleSize contentWidth = Prelude.round k
+			     where
+			     f=calcFontSize contentWidth
+			     p= phi ^ 2
+			     k= realToFrac(f) * p 
 
 -- | returns Css for Calculated title size based on contentWidth.
-grTitleSize::  Css
-grTitleSize = do
+grTitleSize:: Integer -> Css
+grTitleSize contentWidth = do
 
 	fontSize(px k)
 	fontSize (em p)
 	lineHeight (em c)
 
 	where
-	k = calcTitleSize
-	f = calcFontSize
+	k = calcTitleSize contentWidth
+	f = calcFontSize contentWidth
 	p = (realToFrac((100*k)) / (realToFrac(f))) 
 	c = (realToFrac(k)) / yoo
 
+
+
 --  | Calculated Headline Size based on contentWidth.
-calcHeadLineSize:: Integer
-calcHeadLineSize= Prelude.round k
-		  where
-		  f=calcFontSize 
-		  p= phi ^ 1
-		  k= realToFrac(f) * p 
+calcHeadLineSize:: Integer -> Integer
+calcHeadLineSize contentWidth = Prelude.round k
+		  		where
+		  		f=calcFontSize contentWidth 
+		  		p= phi ^ 1
+		  		k= realToFrac(f) * p 
 
  
 -- | returns Css for Calculated Headline Size based on contentWidth.
-grHeadlineSize:: Css
-grHeadlineSize = do
+grHeadlineSize:: Integer -> Css
+grHeadlineSize contentWidth = do
 	fontSize(px k)
 	fontSize (em p)
 	lineHeight (em c)
 
 	where
-	k = calcHeadLineSize
-	f = calcFontSize
+	k = calcHeadLineSize contentWidth
+	f = calcFontSize contentWidth
 	p = (realToFrac((100*k)) / (realToFrac(f))) 
 	c = (realToFrac(k)) / yoo
+
+
 
  
 --  | Calculated sub-Headline Size based on contentWidth. 
-calcSubHeadLineSize:: Integer
-calcSubHeadLineSize =   Prelude.round k
-			where
-			f=calcFontSize 
-			p= sqrt(phi)
-			k= realToFrac(f) * p 
+calcSubHeadLineSize:: Integer -> Integer
+calcSubHeadLineSize contentWidth =   Prelude.round k
+				     where
+				     f=calcFontSize contentWidth
+				     p= sqrt(phi)
+				     k= realToFrac(f) * p 
 
 
 -- | returns Css for sub-Headline Size based on contentWidth.
-grSubHeadLineSize::  Css
-grSubHeadLineSize = do
+grSubHeadLineSize::  Integer -> Css
+grSubHeadLineSize contentWidth = do
 
 	fontSize(px k)
 	fontSize (em p)
 	lineHeight (em c)
 
 	where
-	k = calcSubHeadLineSize
-	f = calcFontSize
+	k = calcSubHeadLineSize contentWidth
+	f = calcFontSize contentWidth
 	p = (realToFrac((100*k)) / (realToFrac(f))) 
 	c = (realToFrac(k)) / yoo
 
+
 --  | Calculated secondary text size based on contentWidth.
-calcSecondaryText::Integer
-calcSecondaryText = Prelude.round k
-		    where
-		    f=calcFontSize 
-		    p= sqrt(phi)
-		    k= realToFrac(f) * ( (1.0) / p)
+calcSecondaryText:: Integer -> Integer
+calcSecondaryText contentWidth = Prelude.round k
+		    		 where
+		    		 f=calcFontSize contentWidth
+		    		 p= sqrt(phi)
+		    		 k= realToFrac(f) * ( (1.0) / p)
 
 
 -- | returns Css for Calculated secondary text  size based on contentWidth.
-grSecondaryText::  Css
-grSecondaryText = do
+grSecondaryText:: Integer -> Css
+grSecondaryText contentWidth = do
 
 	fontSize(px k)
 	fontSize (em p)
 	lineHeight (em c)
 
 	where
-	k = calcSecondaryText
-	f = calcFontSize
+	k = calcSecondaryText contentWidth
+	f = calcFontSize contentWidth
 	p = (realToFrac((100*k)) / (realToFrac(f))) 
 	c = (realToFrac(k)) / yoo
 
 
+
 --  | Calculated default line-height based on contentWidth.
-calcBaseLineHeight::Double
-calcBaseLineHeight = k
-		     where
-		     f=calcFontSize 
-		     foobar= (sqrt(contentWidth)) / phi
-		     h = (realToFrac(f) * phi) ^ 2
-		     p =((realToFrac(contentWidth)) / h )
-		     foo= realToFrac(f) * (phi - xoo * (1.0 - p))
-		     k= foo / foobar
+calcBaseLineHeight:: Integer -> Double
+calcBaseLineHeight contentWidth = k
+		     		  where
+		     		  f	= calcFontSize contentWidth
+				  j     = realToFrac(contentWidth)
+		     		  foobar= (sqrt(j)) / phi
+		     		  h 	= (realToFrac(f) * phi) ^ 2
+		     		  p 	= ((realToFrac(contentWidth)) / h )
+				  xoo 	= sqrt(j) / phi
+		     		  foo	= realToFrac(f) * (phi - xoo * (1.0 - p))
+		     		  k	= foo / foobar
 
 
 -- | returns Css for Calculated default line-height based on contentWidth.
-grBaseLineHeight::  Css
-grBaseLineHeight = do
+grBaseLineHeight::  Integer -> Css
+grBaseLineHeight contentWidth = do
 
 	lineHeight (em c)
 
 	where
-	c = calcBaseLineHeight
+	c = calcBaseLineHeight contentWidth
+
+
 
 
 --  | Calculated a Golden Ratio line-height based on contentWidth.
-calcCustomLineHeight:: Integer ->Double
-calcCustomLineHeight target =   foo
-				where 
-				foobar= (sqrt(contentWidth)) / phi
-				foo = (realToFrac(target)) / foobar
+calcCustomLineHeight:: Integer -> Integer ->Double
+calcCustomLineHeight contentWidth target =   foo
+					     where
+					     j      = realToFrac(contentWidth)
+					     foobar = (sqrt(j)) / phi
+					     foo    = (realToFrac(target)) / foobar
 
 
 -- | returns Css for Calculated Golden Ratio line-height based on contentWidth.				
-grCustomLineHeight::Integer-> Css
-grCustomLineHeight target = lineHeight (em c)
-		     	    where
-		     	    c = calcCustomLineHeight(target)
+grCustomLineHeight:: Integer -> Integer-> Css
+grCustomLineHeight contentWidth target = lineHeight (em c)
+		     	    		 where
+		     	    		 c = calcCustomLineHeight contentWidth target
+
+
+
+
+
